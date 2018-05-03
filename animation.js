c6b21e61d7564689334c86e6e0e6496ebff04760
Isabella Bilsteen <Isabellabilsteen@Isabellas-MacBook-Pro-2.local>
2018-05-03 10:16:30 +0200

    Lyd og besked fungere nu

--- animation.js
+++ animation.js
@@ -21,6 +21,7 @@ function showScene2() {
     setTimeout(takeOutPhone, 6000);
 }
 
+
 function takeOutPhone() {
     console.log("storm tager telefon frem");
     $("#storm_sprite_1").removeClass("sprite1");
@@ -28,3 +29,38 @@ function takeOutPhone() {
 
 
 }
+
+/* kode for Storms beskeder*/
+
+setTimeout(storm_besked1, 3500);
+setTimeout(storm_svar, 2000);
+setTimeout(storm_besked2, 5000);
+setTimeout(storm_besked3, 7000);
+
+/*sekunderne før næste besked skal starte*/
+
+function storm_svar() {
+    svar.style.display = "block";
+}
+/*viser svar stedet*/
+
+function storm_besked1() {
+    text_1.style.display = "block";
+    $("#besked_lyd_1")[0].play();
+}
+/*viser første besked og starter lyden*/
+
+
+function storm_besked2() {
+    text_2.style.display = "block";
+    $("#besked_lyd_1")[0].play();
+}
+
+/*viser anden besked og starter lyden*/
+
+function storm_besked3() {
+    text_3.style.display = "block";
+    $("#besked_lyd_1")[0].play();
+}
+
+/*viser tredje besked og starter lyden*/
